77434738038815e361612bfb07789aef2f6b9b08
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-05-07 12:46:32 +0530

    Create solution.cpp

---
+++ Day2/300. Longest Increasing Subsequence/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+
+        for(int i=0; i<nums.size(); i++){
+
+            if(sub.empty() || sub[sub.size() - 1] < nums[i]){
+                sub.push_back(nums[i]);
+            }else{
+                auto it = lower_bound(sub.begin(), sub.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
+
+        return sub.size();
+    }
+};
+
+// Time Complexity : O(nlogn)
+// Space Complexity: O(n)
