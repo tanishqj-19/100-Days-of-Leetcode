3848277d174524b86be1521f915d58b4b8270f46
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-05-09 21:21:21 +0530

    Create main.cpp

---
+++ Day4/389. Find the Difference/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(t.begin(), t.end());
+        sort(s.begin(), s.end()) ;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] != t[i])
+                return t[i];
+        }
+        return t[s.size()];
+    }
+};
