bc6d84707edba4fe147b2c9ab19abb237b840297
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-05-23 12:34:33 +0530

    Create stream.cpp

---
+++ Day18/703. Kth Largest Element in a Stream/stream.cpp
@@ -0,0 +1,18 @@
+class KthLargest {
+    priority_queue<int, vector<int> ,greater<int>> pq;
+    int size;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for(int i=0;i <nums.size(); i++){
+            pq.push(nums[i]);
+            if(pq.size() > k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > size) pq.pop();
+        return pq.top();
+    }
+};
