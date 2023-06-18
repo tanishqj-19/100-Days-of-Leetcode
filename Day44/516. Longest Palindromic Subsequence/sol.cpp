ebf489cdd81ebf11710f98d7d3ac7b18d6248330
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-06-18 11:45:10 +0530

    Create sol.cpp

---
+++ Day44/516. Longest Palindromic Subsequence/sol.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string &s) {
+        int n = s.size();
+        string rev = s;
+        reverse(s.begin(), s.end());
+        vector<int> prev(n+1, 0), curr(n+1, 0);
+
+        for(int i =1;i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == rev[j-1])
+                    curr[j] = 1 + prev[j-1];
+                else
+                    curr[j] = max(prev[j], curr[j-1]); 
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+
+
+    }
+};
