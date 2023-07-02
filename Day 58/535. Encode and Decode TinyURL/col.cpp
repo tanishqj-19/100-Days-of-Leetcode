c9a43ba24ae72033fea1bd28bc094c31cd899ddd
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-02 11:25:29 +0530

    Create col.cpp

---
+++ Day 58/535. Encode and Decode TinyURL/col.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    string s = "";
+public:
+
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        s = longUrl;
+        return "";
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return  s;
+    }
+};
