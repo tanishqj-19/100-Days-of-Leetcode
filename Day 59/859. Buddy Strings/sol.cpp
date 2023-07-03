d029b9faefbde0fee4b7e6023c81ffe5df236775
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-03 20:52:15 +0530

    Create sol.cpp

---
+++ Day 59/859. Buddy Strings/sol.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool buddyStrings(string &s, string &goal) {
+        int n = s.size();
+        if(s == goal){
+            set<char> temp(s.begin(), s.end());
+            return temp.size() < goal.size();
+        }
+
+        int i = 0, j = n-1;
+
+        while(i < j && s[i] == goal[i]) i++;
+        while(j >= 0 && s[j] == goal[j]) j--;
+
+        if(i < j)
+            swap(s[i], s[j]);
+
+        return s == goal;
+    }
+};
