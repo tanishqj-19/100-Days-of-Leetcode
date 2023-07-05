c1ecdcd8ad8a45f33dc5c5b2ffdfc5d64a2042c7
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-05 21:33:07 +0530

    Create sol.cpp

---
+++ Day61/414. Third Maximum Number/sol.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> s;
+
+        for(int i=0; i<nums.size(); i++){
+            s.insert(nums[i]);
+            if(s.size() > 3)
+                s.erase(s.begin());
+        }
+
+        if(s.size() == 3)
+            return *s.begin();
+        return *s.rbegin();
+    }
+};
