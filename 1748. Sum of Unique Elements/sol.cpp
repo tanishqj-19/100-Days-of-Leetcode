3e4955ceabbc5e6b06d4c23be985533631c3f3e5
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-07 21:33:42 +0530

    Create sol.cpp

---
+++ 1748. Sum of Unique Elements/sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum = 0;
+        unordered_map<int, int> mp;
+
+        for(int &n: nums)
+            mp[n]++;
+        for(auto & x: mp){
+            if(x.second == 1){
+                sum += x.first;
+
+            }
+        }
+
+        return sum;
+
+    }
+};
