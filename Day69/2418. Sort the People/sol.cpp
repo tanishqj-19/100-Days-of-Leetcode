ce80df0a19dfb3a6df10ea790ea0de28a6e385f6
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-13 16:07:37 +0530

    Create sol.cpp

---
+++ Day69/2418. Sort the People/sol.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        unordered_map<int, string> mp;
+        for(int i=0; i<names.size(); i++){
+            mp[heights[i]] = names[i];
+        }
+
+        sort(heights.begin(), heights.end(), greater<int>());
+
+        for(int i=0; i<heights.size(); i++){
+            names[i] = mp[heights[i]];
+        }
+
+        return names;
+    }
+};
