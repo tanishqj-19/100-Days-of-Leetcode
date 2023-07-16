3b8758743e1bb4e8f3a5083433d72fe912fdf9b3
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-07-16 15:21:20 +0530

    Create sol.cpp

---
+++ Day72/2583. Kth Largest Sum in a Binary Tree/sol.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        vector<long long> ans;
+
+        queue<TreeNode*> q;
+
+        q.push(root);
+        long long  level = 0, temp = 0;
+
+        while(!q.empty()){
+            int n = q.size();
+            level++;
+            temp = 0;
+
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front(); 
+                q.pop();
+                temp += node->val;
+
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+
+            ans.push_back(temp);
+        }
+
+        if(ans.size() < k)
+            return -1;
+        sort(ans.begin(), ans.end(), greater<long long>());
+        return ans[k-1];
+    }
+};
