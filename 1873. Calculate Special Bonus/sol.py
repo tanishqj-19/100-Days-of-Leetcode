95202ba16d7b9927b2553cfed239a735ff454d49
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-08-06 12:21:50 +0530

    Delete 1873. Calculate Special Bonus directory

--- 1873. Calculate Special Bonus/sol.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] = employees.apply(lambda x: x['salary'] if int(x['employee_id']) % 2 != 0 and x['name'][0] != 'M' else 0, axis = 1)
-
-    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
