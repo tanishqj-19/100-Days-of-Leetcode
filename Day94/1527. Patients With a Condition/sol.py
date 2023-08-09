1dada263441c93352ccb04229c9296a5335e30ac
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-08-09 18:35:29 +0530

    Create sol.py

---
+++ Day94/1527. Patients With a Condition/sol.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    patients = patients[patients.conditions.str.contains(r'(^DIAB1)|( DIAB1)') ] 
+
+    return patients
