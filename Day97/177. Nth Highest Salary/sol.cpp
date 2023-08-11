b3150b6dc8a761db22b694ab1dab71671222b6e7
Tanishq Jain <92851477+tanishqj-19@users.noreply.github.com>
2023-08-11 12:05:28 +0530

    Create sol.cpp

---
+++ Day97/177. Nth Highest Salary/sol.cpp
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    unique_salaries = employee.salary.unique()
+    if len(unique_salaries) < N:
+        return pd.DataFrame([np.NaN], columns=[f'getMthHighestSalary({N})'])
+    else:
+        salary = sorted(unique_salaries, reverse=True)[N-1]
+        return pd.DataFrame([salary], columns=[f'getMthHighestSalary({N})'])
